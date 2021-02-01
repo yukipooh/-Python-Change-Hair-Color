efba980772ad72076f06e7d8caf6414719cebd81
yuki_pooh <yuki@pooh-san.no-ip.com>
2021-02-01 10:54:05 +0900

    不要なファイルの削除

--- Scripts/codetest.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-ndr = np.ndarray((2,5))
-for i in range(2):
-    for j in range(5):
-        ndr[i,j] = j
-print(ndr.item(9))
--- Scripts/test.py
+++
@@ -1,8 +0,0 @@
-def f(x):
-    return x**4-2*x**3+x**2-2*x
-
-
-
-for i in range(10):
-    tmp = i - 3
-    print(f"{tmp}:{f(tmp)}")
